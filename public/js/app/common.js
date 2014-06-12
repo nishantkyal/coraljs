9084673e2bc44234cc5061e8bd73e7dfb6fc0ae4
Nishant <nishant.kyal@gmail.com>
2014-06-12 14:04:34 +0000

    Create and edit coupon implented

---
+++ public/js/app/common.js
@@ -0,0 +1,16 @@
+function populate(frm, data) {
+    $.each(data, function(key, value){
+        $('[name='+key+']', frm).val(value);
+    });
+}
+
+function getFormData(form)
+{
+    var data = {};
+    _.each($('input,select', form), function(element)
+    {
+        data[$(element).attr('name')] = $(element).val();
+    });
+
+    return data;
+}
