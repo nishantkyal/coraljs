8b6c0f248c8fe42028a239f10c3abe755f1fd426
Nishant <nishant.kyal@gmail.com>
2014-06-19 06:40:38 +0000

    Debug

--- routes/dashboard/DashboardRoute.ts
+++ routes/dashboard/DashboardRoute.ts
@@ -226,7 +226,7 @@ class DashboardRoute
         var self = this;
         var sessionData = new SessionData(req);
         var pageData = sessionData.getData();
-        var userId = parseInt(req[ApiConstants.USER]);
+        var userId = parseInt(req[ApiConstants.USER].id);
 
         q.all([
             self.expertiseDelegate.search(Utils.createSimpleObject(Expertise.USER_ID, userId))
