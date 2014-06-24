daab2b9d4e45389edb9acad7f4e84ca7fcbc869f
Nishant <nishant.kyal@gmail.com>
2014-06-24 07:26:12 +0000

    Logout doesn't change page unless it's a dashboard page

--- app.ts
+++ app.ts
@@ -95,10 +95,13 @@ app.use(
         // This middleware applies to all urls except
         // 1. APIs (which start with "/rest")
         // 2. Static content (which start with "/js" or "/css" or "/img")
-        var excludeRegex = /^\/(rest|css|js|images|img|fonts)/;
+        var excludeRegex = /^\/(rest|bower_dependencies|css|js|images|img|fonts)/;
 
         if (Utils.isNullOrEmpty(req.path.match(excludeRegex)))
+        {
             _.extend(res.locals, helpers);
+            res.locals.path = req.path;
+        }
 
         next();
     }
