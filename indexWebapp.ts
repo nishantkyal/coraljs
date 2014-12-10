b6c56763e740742ba0076fa1d48ad333f69d7520
Ankit <ankitag9@gmail.com>
2014-12-10 16:39:43 +0530

    Separate index file for webapp

---
+++ indexWebapp.ts
@@ -0,0 +1,40 @@
+///<reference path='./_references.d.ts'/>
+import CountryCode = require("./enums/CountryCode");
+import CountryName = require("./enums/CountryName");
+import DayName = require("./enums/DayName");
+import ForeignKeyType = require("./enums/ForeignKeyType");
+import ImageSize = require("./enums/ImageSize");
+import IndustryCode = require("./enums/IndustryCode");
+import MoneyUnit = require("./enums/MoneyUnit");
+import PhoneType = require("./enums/PhoneType");
+import SMSStatus = require("./enums/SMSStatus");
+import Salutation = require("./enums/Salutation");
+import ServiceRequestStatus = require("./enums/ServiceRequestStatus");
+import AbstractModel = require("./models/AbstractModel");
+import BaseModel = require("./models/BaseModel");
+import ForeignKey = require("./models/ForeignKey");
+import Formatter = require("./common/Formatter");
+import Utils = require("./common/Utils");
+import sqlToModel = require("./common/sqlToModel");
+
+var exported = {
+    CountryCode: CountryCode,
+    CountryName: CountryName,
+    DayName: DayName,
+    ForeignKeyType: ForeignKeyType,
+    ImageSize: ImageSize,
+    IndustryCode: IndustryCode,
+    MoneyUnit: MoneyUnit,
+    PhoneType: PhoneType,
+    SMSStatus: SMSStatus,
+    Salutation: Salutation,
+    ServiceRequestStatus: ServiceRequestStatus,
+    AbstractModel: AbstractModel,
+    BaseModel: BaseModel,
+    ForeignKey: ForeignKey,
+    Formatter: Formatter,
+    Utils: Utils,
+    sqlToModel: sqlToModel,
+};
+
+export = exported
