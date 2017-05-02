db53b32b6f506377e50b1b08188ac7b794d0cede
Nishant Kyal <nishant.kyal@snapdeal.com>
2017-05-02 18:18:58 +0530

    Upgraded version for npm

---
+++ index.js
@@ -0,0 +1,57 @@
+///<reference path='./_references.d.ts'/>
+var CountryCode = require("./enums/CountryCode");
+var CountryName = require("./enums/CountryName");
+var DayName = require("./enums/DayName");
+var ForeignKeyType = require("./enums/ForeignKeyType");
+var ImageSize = require("./enums/ImageSize");
+var Salutation = require("./enums/Salutation");
+var AbstractModel = require("./models/AbstractModel");
+var BaseModel = require("./models/BaseModel");
+var BaseS3Model = require("./models/BaseS3Model");
+var ForeignKey = require("./models/ForeignKey");
+var SolrDao = require("./dao/SolrDao");
+var S3Dao = require("./dao/S3Dao");
+var MysqlDao = require("./dao/MysqlDao");
+var BaseMappingDao = require("./dao/BaseMappingDao");
+var BaseDaoDelegate = require("./delegates/BaseDaoDelegate");
+var BaseApi = require("./api/BaseApi");
+var BaseMappingDaoDelegate = require("./delegates/BaseMappingDaoDelegate");
+var FileWatcherDelegate = require("./delegates/FileWatcherDelegate");
+var GlobalIDDelegate = require("./delegates/GlobalIDDelegate");
+var ImageDelegate = require("./delegates/ImageDelegate");
+var LocalizationDelegate = require("./delegates/LocalizationDelegate");
+var MysqlDelegate = require("./delegates/MysqlDelegate");
+var Formatter = require("./common/Formatter");
+var Utils = require("./common/Utils");
+var sqlToModel = require("./common/sqlToModel");
+var CacheHelper = require("./caches/CacheHelper");
+var exported = {
+    CountryCode: CountryCode,
+    CountryName: CountryName,
+    DayName: DayName,
+    ForeignKeyType: ForeignKeyType,
+    ImageSize: ImageSize,
+    Salutation: Salutation,
+    AbstractModel: AbstractModel,
+    BaseModel: BaseModel,
+    BaseS3Model: BaseS3Model,
+    ForeignKey: ForeignKey,
+    SolrDao: SolrDao,
+    S3Dao: S3Dao,
+    MysqlDao: MysqlDao,
+    BaseMappingDao: BaseMappingDao,
+    BaseDaoDelegate: BaseDaoDelegate,
+    BaseApi: BaseApi,
+    BaseMappingDaoDelegate: BaseMappingDaoDelegate,
+    FileWatcherDelegate: FileWatcherDelegate,
+    GlobalIDDelegate: GlobalIDDelegate,
+    ImageDelegate: ImageDelegate,
+    LocalizationDelegate: LocalizationDelegate,
+    MysqlDelegate: MysqlDelegate,
+    Formatter: Formatter,
+    Utils: Utils,
+    sqlToModel: sqlToModel,
+    CacheHelper: CacheHelper
+};
+module.exports = exported;
+//# sourceMappingURL=index.js.map
